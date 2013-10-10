ad3193a78a00325bd783d8abffe917d72d303a26
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-10-10 13:52:26 +0300

    Added find_translations

--- sqlalchemy_i18n/__init__.py
+++ sqlalchemy_i18n/__init__.py
@@ -32,3 +32,46 @@ def make_translatable(
     sa.event.listen(
         session, 'before_flush', manager.auto_create_translations
     )
+
+
+def find_translations(obj, property_name, locale):
+    class_ = obj.__class__
+    session = sa.orm.object_session(obj)
+    translation_class = class_.__translatable__['class']
+    manager = class_.__translatable__['manager']
+
+    property_ = getattr(translation_class, property_name)
+
+    subquery = (
+        session.query(translation_class.id)
+        .filter(
+            sa.and_(
+                property_ ==
+                getattr(obj, property_name),
+                translation_class.locale ==
+                manager.option(class_, 'default_locale')
+            )
+        )
+    )
+
+    conditions = [
+        translation_class.id.in_(subquery),
+        translation_class.locale == locale,
+        property_ != None
+    ]
+
+    total_count = (
+        session.query(sa.func.cast(sa.func.count('1'), sa.Numeric))
+        .filter(sa.and_(*conditions))
+    )
+
+    query = (
+        session.query(
+            property_.label('translation'),
+            (sa.func.cast(sa.func.count('1'), sa.Numeric) / total_count)
+            .label('confidence')
+        )
+        .filter(sa.and_(*conditions))
+        .group_by(property_)
+    )
+    return query
