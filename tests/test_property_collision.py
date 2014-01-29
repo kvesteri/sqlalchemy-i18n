c6f2c0a6aea053de7d025877e1c98095b192d79f
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-01-29 14:33:47 +0200

    Combine test cases

--- tests/test_property_collision.py
+++ tests/test_property_collision.py
@@ -35,3 +35,26 @@ class TestPropertyCollision(TestCase):
     def test_raises_exception(self):
         with raises(ImproperlyConfigured):
             self.Article()
+
+
+class TestPropertyCollisionWithSingleTableInheritance(TestCase):
+    def create_models(self):
+        class TextItem(self.Model, Translatable):
+            __tablename__ = 'article'
+            __translated_columns__ = [
+                sa.Column('name', sa.Unicode(255))
+            ]
+
+            id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+
+            def get_locale(self):
+                return 'en'
+
+        class Article(TextItem):
+            description = sa.Column(sa.UnicodeText)
+
+        self.TextItem = TextItem
+        self.Article = Article
+
+    def test_model_initialization(self):
+        self.Article()
